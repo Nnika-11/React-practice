2ed8e632d62234aa9bb9641423ff8628603b5cb1
Nnika-11 <sherr.veronika@gmail.com>
2021-02-04 13:24:46 +0300

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default class Header extends Component {
+    render(){
+      <div></div> 
+    }
+}
